58b7c3106a3bff19f49d3bc0a678b537cb4b1127
Grilloz <grillo.davide@gmail.com>
2020-05-09 15:52:21 +0200

    first commit

---
+++ src/pages/[test].svelte
@@ -0,0 +1,9 @@
+<script>
+  export let test;
+
+  $: title = decodeURI(test);
+</script>
+
+<h1> {title} </h1>
+<p>This is a test</p>
+<p>param is {title}</p>
---
+++ src/pages/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    
+  import { url } from '@sveltech/routify';
+  let testlink = 'My Test';
+
+</script>
+
+<a href={$url('/prova')}>Prova</a>
+<a href={$url('/:test', {test: testlink})}>{testlink}</a>
+
